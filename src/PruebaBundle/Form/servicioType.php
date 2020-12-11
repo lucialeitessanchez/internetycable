621dad742c1cde2b8f68c2e520f1a18ec4092a19
lucialeitessanchez <lucialeitessanchez@gmail.com>
2020-12-11 15:01:01 -0300

    creacion del formulario, en nuevo y boton guardar en bd

---
+++ src/PruebaBundle/Form/servicioType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PruebaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class servicioType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('referencia')->add('compania')->add('direccion')->add('ciudad')->add('estado');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PruebaBundle\Entity\servicio'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'pruebabundle_servicio';
+    }
+
+
+}
