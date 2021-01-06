da388999b30ca7e5149d2e8f4372a845ceb95243
lucialeitessanchez <lucialeitessanchez@gmail.com>
2021-01-06 16:00:42 -0300

    parte de creacion de usuario, el registro

---
+++ src/PruebaBundle/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PruebaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username')->add('email')->add('password');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PruebaBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'pruebabundle_user';
+    }
+
+
+}
