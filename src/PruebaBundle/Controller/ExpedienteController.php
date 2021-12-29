c1b81b7c78b3c5207aec7d5bf355119ee75e2ed7
lucialeitessanchez <lucialeitessanchez@gmail.com>
2021-12-29 15:49:42 -0300

    empece el pdf

--- src/PruebaBundle/Controller/ExpedienteController.php
+++ src/PruebaBundle/Controller/ExpedienteController.php
@@ -178,39 +178,88 @@ class ExpedienteController extends Controller
 
 }
 
-    public function transPDFAction()
-    {
-        $pdf = new TCPDF('L');
-        $pdf->SetPrintHeader(true);
-        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-        // set margins
-        $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-        // set auto page breaks
-        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-        // set image scale factor
-        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-        // set some language-dependent strings (optional)
-        if (@file_exists(dirname(__FILE__) . '/lang/eng.php')) {
-            require_once dirname(__FILE__) . '/lang/eng.php';
-            $pdf->setLanguageArray(L);
-        }
-        // set font
-        $pdf->SetFont('helvetica', '', 8);
-        // add a page
-        $pdf->AddPage();
-        //get report data into $data variable
-        $project = $this->getServiceLocator()->get('ProjectTable');
-        $data = $project->getWsr();
-        $view = new PhpRenderer();
-        $resolver = new TemplateMapResolver();
-        //set the path of the pdf.phtml file
-        $resolver->setMap(array('PDFTemplate' => '/var/www/html/WSRAutomation/module/Application/view/application/index/pdf.phtml'));
-        $view->setResolver($resolver);
-        $viewModel = new ViewModel();
-        $viewModel->setTemplate('PDFTemplate')->setVariables(array('projects' => $data, 'view' => 'pdf'));
-        $html = $view->render($viewModel);
-        $pdf->writeHTML($html, true, 0, true, 0);
-        $pdf->Output('WsrReport.pdf', 'I');
+ public function expedientePDFAction(Request $request,int $id){
+
+    //base de datos
+    $em = $this->getDoctrine()->getManager();
+    $expediente = $em->getRepository('PruebaBundle:Expediente')->find($id); //obtengo el objeto expediente
+    $factura = $expediente->getFacturas(); //obtengo el objeto factura relacionado a ese expediente
+
+    $numFactura = $factura->getNumFactura();
+
+    $pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false); //llamo a la clase que extiende tcpdf
+    $pdf->AddPage();
+    $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT); //margenes
+
+    //saber de donde viene el expediente
+    $numE=substr($expediente->getNumeroExpe(),0,1);
+    if($numE == 0){
+        $txt="Ref.: Expte. Nro 0".$expediente->getNumeroExpe();
+        
+    } 
+    //echo $numE;
+
+    //transformo la fecha en texto y en español
+    $fechaActual = date('d M Y');
+    $fechaActual = $this->fechaCastellano($fechaActual);
+    
+    //textos derecha
+    $txt="Ref.: Expte. Nro 0".$expediente->getNumeroExpe();
+    $txt2="Santa Fe, ".$fechaActual;
+    $pdf->Write(0, $txt, '', 0, 'R', true, 0, false, false, 0);
+    $pdf->Write(0, $txt2, '', 0, 'R', true, 0, false, false, 0);
+
+    //espacios
+
+    $pdf->Write(3, ' ', '', 0, 'R', true, 0, false, false, 0); 
+    $pdf->Write(3, ' ', '', 0, 'R', true, 0, false, false, 0);
+    $pdf->Write(3, ' ', '', 0, 'R', true, 0, false, false, 0); 
+
+    //parrafo principal
+    $txt3="Se Informa que corresponde a la factura n°: ".$numFactura;
+    $txt4="por el Servicio de ";
+    $txt5="del período de ";
+    $txt6=" prestado por la empresa ";
+    $txt7=" en la dependecia de este Ministerio de Igualdad, Género y Diversidad de calle "; //fijarse si es dependencia o no
+    
+    //$txt4="por el Servicio de".$factura->getService();
+    $pdf->Write(0, $txt3, '', 0, 'J', true, 0, false, false, 0);
+
+
+    //salida PDF
+    $pdf->Output('nota.pdf', 'I');
+
+ }
+}
+ class MYPDF extends \TCPDF {
+
+    //Page header
+    public function Header() {
+        // Logo
+       // $image_file = 'imagenes/logo ministerio-negro_sin fondo.png';
+       // $this->Image($image_file, 10, 10, 15, '', 'PNG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+        // Set font
+        $this->SetFont('helvetica', 'B', 20);
+        // Title
+        //$this->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+    }
+
+    // Page footer
+    public function Footer() {
+        // Position at 15 mm from bottom
+        $this->SetY(-15);
+        // Set font
+        $this->SetFont('helvetica', 'I', 8);
+        //$image_file = 'imagenes/logo ministerio-negro_sin fondo.png';
+        $this->Image('imagenes/logo ministerio-negro_sin fondo.png');
+        // Footer
+        $this->Cell(0, 10, 'Sectorial de Informática
+        Ministerio de Igualdad, Género y Diversidad
+        Corrientes 2879 - (3000) Santa Fe
+        Tel: (0342) 4572888 / 4589468', 0, false, 'C', 0, '', 0, false, 'T', 'M');
+        
     }
 }
+
+
+
