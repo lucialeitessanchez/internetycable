69bea3793a5c308510b60b4055bcafe4eb2df2c2
lucialeitessanchez <lucialeitessanchez@gmail.com>
2022-01-28 10:01:19 -0300

    habia un 0 de mas que rompia todo

--- src/PruebaBundle/Controller/ExpedienteController.php
+++ src/PruebaBundle/Controller/ExpedienteController.php
@@ -443,7 +443,7 @@ function expedientePDFOne(Expediente $expediente,Factura $factura){
         foreach($data as $row) {
             $this->Cell($w[0], 6, $row[0], 'LR', 0, 'L', $fill);
             $this->Cell($w[1], 6, $row[1], 'LR', 0, 'L', $fill);
-            0$this->Cell($w[2], 6, number_format($row[2]), 'LR', 0, 'R', $fill);
+            $this->Cell($w[2], 6, number_format($row[2]), 'LR', 0, 'R', $fill);
             $this->Cell($w[3], 6, number_format($row[3]), 'LR', 0, 'R', $fill);
             $this->Ln();
             $fill=!$fill;
