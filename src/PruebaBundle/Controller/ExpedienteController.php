7738a3429c797d35287aa7a6d00ac07c3f3d237b
lucialeitessanchez <lucialeitessanchez@gmail.com>
2022-02-10 12:58:45 -0300

    empece con el pdf varias facturas, falta mejorar visual

--- src/PruebaBundle/Controller/ExpedienteController.php
+++ src/PruebaBundle/Controller/ExpedienteController.php
@@ -341,11 +341,22 @@ function expedientePDFOne(Expediente $expediente,Factura $factura){
         //parrafo principal
         $txt3="Se Informa los siguientes servicios que fueron prestados a este Ministerio de Igualdad, Genero y Diversidad, y sus dependencias, en el siguiente cuadro:";
         
+
+          //cuadro
+          
+          foreach($facturas as $factura){
+              $servicios=$factura->getService();
+            foreach($servicios as $servicio){
+                        $data[] = [$factura->getNumFactura(),$factura->getPeriodo(),$servicio->getReferencia(),$servicio->getDireccion(),$servicio->getCiudad(),$servicio->getTipo()];
+                    }
+                
+                    
+                }
         // column titles
-        $header = array('Factura nº', 'Periodo', 'Número de referencia', 'Dirección');
+        $header = array('Factura nº','Periodo','Nº de referencia','Dirección','Ciudad','Tipo de servicio');
 
         // print colored table
-        $pdf->ColoredTable($header, $data);
+        $pdf->ColoredTableF($header, $data);
     }
 
    
@@ -412,6 +423,39 @@ function expedientePDFOne(Expediente $expediente,Factura $factura){
             $this->Cell($w[1], 6, $row[1], 'LR', 0, 'L', $fill);
             $this->Cell($w[2], 6, ($row[2]), 'LR', 0, 'R', $fill);
             $this->Cell($w[3], 6, ($row[3]), 'LR', 0, 'R', $fill);
+            $this->Ln();
+            $fill=!$fill;
+        }
+        $this->Cell(array_sum($w), 0, '', 'T');
+    }
+
+    public function ColoredTableF($header,$data) {
+        // Colors, line width and bold font
+        $this->SetFillColor(255, 0, 0);
+        $this->SetTextColor(255);
+        $this->SetDrawColor(128, 0, 0);
+        $this->SetLineWidth(0.3);
+        $this->SetFont('', 'B');
+        // Header
+        $w = array(35, 25 , 35, 40,25,25);
+        $num_headers = count($header);
+        for($i = 0; $i < $num_headers; ++$i) {
+            $this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', 1);
+        }
+        $this->Ln();
+        // Color and font restoration
+        $this->SetFillColor(224, 235, 255);
+        $this->SetTextColor(0);
+        $this->SetFont('');
+       //recorro el arreglo con la info y por cada elemento lo voy colocando en las dif. columnas
+        $fill = 0;
+        foreach($data as $row) {
+            $this->Cell($w[0], 6, $row[0], 'LR', 0, 'L', $fill);
+            $this->Cell($w[1], 6, $row[1], 'LR', 0, 'L', $fill);
+            $this->Cell($w[2], 6, ($row[2]), 'LR', 0, 'R', $fill);
+            $this->Cell($w[3], 6, ($row[3]), 'LR', 0, 'R', $fill);
+            $this->Cell($w[4], 6, ($row[4]), 'LR', 0, 'R', $fill);
+            $this->Cell($w[5], 6, ($row[5]), 'LR', 0, 'R', $fill);
             $this->Ln();
             $fill=!$fill;
         }
